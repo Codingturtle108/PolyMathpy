efb465eeeca866e3fa546bc42b61eba98429e224
Codingturtle108 <black9lightning9@gmail.com>
2023-09-03 18:20:29 +0530

    Added Add operation to Polynomial Class Currently Function valid only for Polynimials of the Same Degree and Consisting of the Same Variable

--- Classdefn/classdef.py
+++ Classdefn/classdef.py
@@ -15,5 +15,14 @@ class Polynomials:
                 else:
                     self.Polynomial += f'+{self.coeff[i]}{self.x}**{i}'
         return self.Polynomial
-p1 = Polynomials([1,2,1],2,'x')
-print(p1.genPoly())
+    def __add__(p1, p2): #Adding Add Operation In Polynomial Class
+        # Assuming Degree to Be Same
+        degree_res = max(p1.degree, p2.degree)
+        print(degree_res)
+        coeff_res = [0]*(degree_res+1)
+        for i in range(degree_res+1):
+            coeff_res[i] += p1.coeff[i] + p2.coeff[i]
+        print(coeff_res)
+        # For now Assuming The Polynomials consist of the same variable
+        Poly_res = Polynomials(coeff_res,degree_res,p1.x)
+        return Poly_res
